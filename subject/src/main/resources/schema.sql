68693ec93b789358b59dfe689eb1aac6f0261a69
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-08-30 21:44:23 +0200

    schema.sql and connection to database

---
+++ subject/src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS subject
+(
+    id      integer primary key generated always as identity,
+   -- userid  INT,
+    name VARCHAR(255)
+   -- fork    boolean,
+ --   name    VARCHAR(255),
+ --   login   VARCHAR(255),
+ --   branch  text[],
+ --   sha     VARCHAR(255)
+    );
