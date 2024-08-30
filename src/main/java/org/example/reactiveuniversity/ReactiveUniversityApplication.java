3b389099375725686cf41cffae24e18b4d1a974a
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-08-30 21:02:57 +0200

    Create Project

---
+++ src/main/java/org/example/reactiveuniversity/ReactiveUniversityApplication.java
@@ -0,0 +1,13 @@
+package org.example.reactiveuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveUniversityApplication.class, args);
+    }
+
+}
