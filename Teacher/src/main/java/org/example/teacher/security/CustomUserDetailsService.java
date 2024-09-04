1b891411b71dcc6c9b08005cfcb070962ef4f0a2
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-09-04 20:56:07 +0200

    security teacher

---
+++ Teacher/src/main/java/org/example/teacher/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package org.example.teacher.security;
+
+import org.example.teacher.TeacherServices;
+import org.example.teacher.dto.TeacherLogin;
+import org.example.teacher.exception.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final TeacherServices teacherServices;
+
+    public CustomUserDetailsService(TeacherServices teacherServices) {
+        this.teacherServices = teacherServices;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return teacherServices.login(username).map(
+                this::createUserDetails).switchIfEmpty(Mono.error( new UsernameNotFoundException(String.format("User with email %s not found", username)))).block();
+
+    }
+
+    private UserDetails createUserDetails(TeacherLogin login) {
+        return User.builder()
+                .username(login.email())
+                .password(login.password())
+                //.roles(credentials.roles().toArray(String[]::new))
+                .build();
+    }
+
+
+}
---
+++ Teacher/src/main/java/org/example/teacher/security/JwtAuthenticationException.java
@@ -0,0 +1,9 @@
+package org.example.teacher.security;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+}
