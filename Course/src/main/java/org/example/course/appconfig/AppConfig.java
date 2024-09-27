dfab19c1d0c4e7ff7dab717cf05919fab6a0421a
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-09-27 19:12:58 +0200

    course  security

--- Course/src/main/java/org/example/course/appconfig/AppConfig.java
+++ Course/src/main/java/org/example/course/appconfig/AppConfig.java
@@ -1,15 +1,33 @@
 package org.example.course.appconfig;
 
+import org.example.course.security.BearerTokenFilter;
+import org.example.course.security.JwtService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpMethod;
 import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.nio.file.Files;
 @Configuration
 public class AppConfig {
+
+    public AppConfig(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    private final JwtService jwtService;
     @Bean
     public ApplicationRunner initializeDatabase(DatabaseClient databaseClient) {
         return sql -> {
@@ -18,5 +36,29 @@ public class AppConfig {
             schemaSql = new String(Files.readAllBytes(resource.getFile().toPath()));
             databaseClient.sql(schemaSql).then().subscribe();
         };
+    }
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+
+
+        BearerTokenFilter bearerTokenFilter = new BearerTokenFilter(jwtService);
+
+
+        http.authorizeHttpRequests(requests -> requests.requestMatchers(mvc.pattern(HttpMethod.POST, "/teacher")).permitAll().requestMatchers(mvc.pattern(HttpMethod.POST, "/teacher/update")).hasAnyRole("Teacher", "Office").requestMatchers(mvc.pattern(HttpMethod.GET, "/teacher/{email}/name")).hasAnyRole("Teacher","Office").anyRequest().authenticated());
+        http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.addFilterBefore(bearerTokenFilter, AuthorizationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
