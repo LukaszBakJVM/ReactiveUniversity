d2b45c13cb3a50ff715a16603a325ee7b08af4b5
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-10-14 19:43:43 +0200

    student security

--- Student/src/main/java/org/example/student/appconfig/AppConfig.java
+++ Student/src/main/java/org/example/student/appconfig/AppConfig.java
@@ -1,16 +1,35 @@
 package org.example.student.appconfig;
 
+import org.example.student.security.BearerTokenFilter;
+import org.example.student.security.JwtService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpMethod;
 import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import java.nio.file.Files;
 
 @Configuration
 public class AppConfig {
+    private final JwtService jwtService;
+
+    public AppConfig(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
 
 
     @Bean
@@ -22,5 +41,18 @@ public class AppConfig {
             databaseClient.sql(schemaSql).then().subscribe();
         };
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+
+        BearerTokenFilter bearerTokenFilter = new BearerTokenFilter(jwtService);
+        http.authorizeHttpRequests(requests -> requests.requestMatchers(mvc.pattern(HttpMethod.POST, "/student")).hasRole("Office").requestMatchers(mvc.pattern(HttpMethod.POST, "/student/update")).hasAnyRole("Office").anyRequest().permitAll());
+
+        http.sessionManagement(sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.addFilterBefore(bearerTokenFilter, AuthorizationFilter.class);
+        return http.build();
+    }
+
 
 }
