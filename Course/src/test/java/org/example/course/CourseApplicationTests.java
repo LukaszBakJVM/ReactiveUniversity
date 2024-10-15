117af27626d3a3ae2ae9a9f87eb16643d07e89ee
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-10-15 19:24:21 +0200

    @test container course

--- Course/src/test/java/org/example/course/CourseApplicationTests.java
+++ Course/src/test/java/org/example/course/CourseApplicationTests.java
@@ -1,10 +1,35 @@
 package org.example.course;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class CourseApplicationTests {
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:latest"
+    ).withInitScript("schema.sql");
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    WebTestClient webTestClient;
+    @BeforeAll
+   static void  startPostgres(){
+        postgres.start();
+
+    }
+    @AfterAll
+    static void stopPostgres(){
+        postgres.stop();
+    }
+
+
 
     @Test
     void contextLoads() {
