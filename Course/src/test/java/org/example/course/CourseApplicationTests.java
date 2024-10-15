eb70f606e013a3d765ddef234a2e6dede436025b
Łukasz Bąk <lukasz.bak@interiowy.pl>
2024-10-15 20:11:10 +0200

    createCourse_shouldReturnCreated_whenUserIsAuthorized

--- Course/src/test/java/org/example/course/CourseApplicationTests.java
+++ Course/src/test/java/org/example/course/CourseApplicationTests.java
@@ -1,38 +1,57 @@
 package org.example.course;
 
+import org.example.course.dto.CourseDto;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.Set;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 class CourseApplicationTests {
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
-            "postgres:latest"
-    ).withInitScript("schema.sql");
-    @Autowired
-    private CourseRepository courseRepository;
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest").withInitScript("schema.sql");
     @Autowired
     WebTestClient webTestClient;
+    @Autowired
+    private CourseRepository courseRepository;
+
     @BeforeAll
-   static void  startPostgres(){
+    static void startPostgres() {
         postgres.start();
 
     }
+
     @AfterAll
-    static void stopPostgres(){
+    static void stopPostgres() {
         postgres.stop();
     }
 
+    @Test
+    @WithMockUser(roles = {"Office"})
+    void createCourse_shouldReturnCreated_whenUserIsAuthorized() {
+        webTestClient // Mock a bearer token
+                .post().uri("/course").contentType(MediaType.APPLICATION_JSON).bodyValue(courseDto()).exchange().expectStatus().isCreated();
+
+
+    }
 
 
     @Test
-    void contextLoads() {
+    void createCourse_shouldReturnForbidden() {
+        webTestClient.post().uri("/course").contentType(MediaType.APPLICATION_JSON).bodyValue(courseDto()).exchange().expectStatus().isForbidden();
+    }
+
+    CourseDto courseDto() {
+        Set<String> subjects = Set.of("Historia", "Matematyka");
+        return new CourseDto("Ścisły", subjects);
     }
 
 }
