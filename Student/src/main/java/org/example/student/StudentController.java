3190ab318686b5a6a2a0527da68e180fda967407
Łukasz Bąk <bbzzyyczczeek@interia.pl>
2024-11-08 10:30:48 +0100

    Response status all application (remove response entity)

--- Student/src/main/java/org/example/student/StudentController.java
+++ Student/src/main/java/org/example/student/StudentController.java
@@ -1,12 +1,10 @@
 package org.example.student;
 
 import org.example.student.dto.*;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/student")
@@ -19,33 +17,39 @@ public class StudentController {
     }
 
     @PostMapping
-    Mono<ResponseEntity<WriteNewPerson>> addStudent(@RequestBody WriteNewPerson student) {
-        return studentServices.createStudent(student).map(person -> ResponseEntity.created(URI.create("/student")).body(person));
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<WriteNewPerson> addStudent(@RequestBody WriteNewPerson student) {
+        return studentServices.createStudent(student);
     }
 
-    @PostMapping("/update")
-    Mono<ResponseEntity<AddCourse>> addCourseToStudent(@RequestBody AddCourse writeCourse) {
-        return studentServices.addCourseToStudent(writeCourse).map(course -> ResponseEntity.created(URI.create("/student/update")).body(course));
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    Mono<AddCourse> addCourseToStudent(@RequestBody AddCourse writeCourse) {
+        return studentServices.addCourseToStudent(writeCourse);
 
     }
 
     @GetMapping("/{email}")
+    @ResponseStatus(HttpStatus.OK)
     Flux<StudentInfoWithCourse> findStudentsByEmail(@PathVariable String email) {
         return studentServices.findStudentsByEmail(email);
     }
 
     @GetMapping("/assigned")
+    @ResponseStatus(HttpStatus.OK)
     Flux<StudentInfoWithCourse> allStudentWithCourse() {
         return studentServices.allStudentsWithCourse();
 
     }
 
     @GetMapping("/unassigned")
+    @ResponseStatus(HttpStatus.OK)
     Flux<StudentInfoWithoutCourse> allStudentWithoutCurses() {
         return studentServices.allStudentsWithoutCourse();
     }
 
     @GetMapping("/write-grades/{email}")
+    @ResponseStatus(HttpStatus.OK)
     Flux<StudentEmail> studentsEmail(@PathVariable String email) {
         return studentServices.findStudentsByEmails(email);
     }
