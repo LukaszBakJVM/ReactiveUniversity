bf1cfa15628fbe0aea06e581478f8c71009fbe14
Łukasz Bąk <bbzzyyczczeek@interia.pl>
2024-11-25 14:56:03 +0100

    registration test

--- src/test/java/org/example/reactiveuniversity/ReactiveUniversityApplicationTests.java
+++ src/test/java/org/example/reactiveuniversity/ReactiveUniversityApplicationTests.java
@@ -4,16 +4,20 @@ import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.example.reactiveuniversity.dto.RegistrationDto;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
@@ -41,10 +45,6 @@ class ReactiveUniversityApplicationTests {
         registry.add("spring.r2dbc.password", postgreSQLContainer::getPassword);
 
     }
-    //  @BeforeEach
-    // void clearDatabase(){
-    //     registrationRepository.deleteAll();
-    // }
 
     @BeforeAll
     static void startPostgres() {
@@ -57,20 +57,96 @@ class ReactiveUniversityApplicationTests {
         postgreSQLContainer.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() {
+        registrationRepository.deleteAll().subscribe();
+
+    }
+
+    // TODO  INTERNAL_SERVER_ERROR
     @Test
-    void createPersonOffice_shouldReturnForbidden_whenUserIsAuthorized_OfficeTeacher() {
+    void createPersonOffice_shouldReturnCreated_whenUserIsAuthorizedOffice() {
+
+        String token = token("lukasz.bak@interiowy.pl", "lukasz");
+        RegistrationDto registration = registration("firstNameOK", "lastNameOk", "email@emialOk.pl", "password", "Office");
+
+        webTestClient.post().uri("/user/registration").header("Authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON).bodyValue(registration).exchange().expectStatus().
+
+                isCreated().expectBody().json(response.registrationDto);
+
+        Mono<Registration> byEmail = registrationRepository.findByEmail(registration.email());
+        StepVerifier.create(byEmail).expectNextCount(1).verifyComplete();
+
+    }
+
+
+    @Test
+    void createPersonOffice_shouldThrowDuplicateEmailException_whenUserIsAuthorizedOffice() {
+
+
+        String token = token("lukasz.bak@interiowy.pl", "lukasz");
+        RegistrationDto registration = registration("firstNameOK", "lastNameOk", "email@emialOk.pl", "password", "Office");
+
+        save().subscribe();
+
+
+        webTestClient.post().uri("/user/registration").header("Authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON).bodyValue(registration).exchange().expectStatus().
+
+                isEqualTo(HttpStatus.CONFLICT).expectBody().json(response.duplicateEmail);
+
+        Mono<Registration> byEmail = registrationRepository.findByEmail(registration.email());
+        StepVerifier.create(byEmail).expectNextCount(1).verifyComplete();
+
+    }
+
+    @Test
+    void createPersonOffice_shouldThrowConnectionException_whenUserIsAuthorizedOffice() {
+
+
+        String token = token("lukasz.bak@interiowy.pl", "lukasz");
+        RegistrationDto registration = registration("firstNameOK", "lastNameOk", "email@emialOk.pl", "password", "Office");
+
+
+        webTestClient.post().uri("/user/registration").header("Authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON).bodyValue(registration).exchange().expectStatus().
+
+                isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR).expectBody().json(response.connectionError);
+
+        Mono<Registration> byEmail = registrationRepository.findByEmail(registration.email());
+        StepVerifier.create(byEmail).expectNextCount(0).verifyComplete();
+
+    }
+
+
+    @Test
+    void createPersonOffice_shouldReturnForbidden_whenUserIsAuthorizedStudent() {
+
+        String token = token("student1@interia.pl", "lukasz");
+        RegistrationDto registration = registration("firstName", "lastName", "email@emial.pl", "password", "Office");
+
+        webTestClient.post().uri("/user/registration").header("Authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON).bodyValue(registration).exchange().expectStatus().isForbidden();
+
+
+        Mono<Registration> byEmail = registrationRepository.findByEmail(registration.email());
+        StepVerifier.create(byEmail).expectNextCount(0).verifyComplete();
+
+    }
+
+    @Test
+    void createPersonOffice_shouldReturnForbidden_whenUserIsAuthorizedTeacher() {
         String token = token("teacher1@interia.pl", "lukasz");
         RegistrationDto registration = registration("firstName", "lastName", "email@emial.pl", "password", "Office");
 
 
         webTestClient.post().uri("/user/registration").header("Authorization", "Bearer " + token).contentType(MediaType.APPLICATION_JSON).bodyValue(registration).exchange().expectStatus().isForbidden();
-        //.isCreated().expectBody().json(response.registration);
 
 
-        //   Mono<Registration> byEmail = registrationRepository.findByEmail(registration.email());
-        //  StepVerifier.create(byEmail).expectNextMatches(office -> office.getFirstName().equals("firstName") && office.getLastName().equals("lastName") && office.getEmail().equals("email@email.com")).verifyComplete();
+        Mono<Registration> byEmail = registrationRepository.findByEmail(registration.email());
+        StepVerifier.create(byEmail).expectNextCount(0).verifyComplete();
 
+    }
 
+    private Mono<Registration> save() {
+        return registrationRepository.save(response.save());
     }
 
     private String token(String email, String password) {
