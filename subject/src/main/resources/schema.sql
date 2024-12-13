7c45bd6171151cabef230b78aba16d75548afd9d
Łukasz Bąk <bbzzyyczczeek@interia.pl>
2024-12-13 13:13:32 +0100

    remove unused method

--- subject/src/main/resources/schema.sql
+++ subject/src/main/resources/schema.sql
@@ -1,11 +1,17 @@
 CREATE TABLE IF NOT EXISTS subject
 (
-    id      integer primary key generated always as identity,
-   -- userid  INT,
-    subject VARCHAR(255)
-   -- fork    boolean,
- --   name    VARCHAR(255),
- --   login   VARCHAR(255),
- --   branch  text[],
- --   sha     VARCHAR(255)
+    id
+    integer
+    primary
+    key
+    generated
+    always as
+    identity,
+
+    subject
+    VARCHAR
+(
+    255
+)
+
     );
